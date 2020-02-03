9a6d1225d4bbead3fd939d0e72c6b5ca8d64b528
p17210836 <p17210836@GWHXX652WIN014W>
2020-02-03 15:11:41 +0000

    Added testing and class for address management

---
+++ Phone Testing/tstStaff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneClasses;
+
+namespace Phone_Testing
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            //create an instance of the class we want to create
+            clsAddress AnAddress = new clsAddress();
+            //test to see that is exists 
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
