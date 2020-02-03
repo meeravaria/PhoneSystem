4b397e6b2c941a95b07eef588242c9a6495660fc
P17192949 <P17192949@GWHXX652WIN015W>
2020-02-03 15:55:37 +0000

    Created properties: DateAdded and Capacity

--- PhoneClasses/clsAddress.cs
+++ PhoneClasses/clsAddress.cs
@@ -1,6 +1,11 @@
-﻿namespace PhoneClasses
+﻿using System;
+
+namespace PhoneClasses
 {
     public class clsAddress
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Capacity { get; set; }
     }
 }
