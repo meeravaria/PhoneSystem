71d05c8e103e6c83bc5bdb56979053308d4da37b
Mohamed Mohamed <P2584197@my365.dmu.ac.uk>
2020-02-10 14:11:19 +0000

    Adding testing and a class for address managment

---
+++ Phone Testing/tstSupplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Phone_Testing
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsAddress AnAddress = new clsAddress();
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
