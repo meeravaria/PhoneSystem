d449b4fc78dcd9527d3b0e2a72a4142042354992
Ashraf Nadeem <nadz156@hotmail.co.uk>
2020-02-10 14:43:17 +0000

    Added testing and class for stock.

---
+++ Phone Testing/tstStock.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneClasses;
+
+namespace Phone_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the new class we want to create
+            clsStock AnStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AnStock);
+        }
+    }
+}
