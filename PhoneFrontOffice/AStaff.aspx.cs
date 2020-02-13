8f053a6eedc7b8a4e917964935905d9d307f8f47
p17210836 <p17210836@GWHXX652WIN014W>
2020-02-13 18:44:29 +0000

    Done week 20 to page 24, need to add the buttons for default page

--- PhoneFrontOffice/AStaff.aspx.cs
+++ PhoneFrontOffice/AStaff.aspx.cs
@@ -19,6 +19,15 @@ public partial class AStaff : System.Web.UI.Page
         clsStaff AStaff = new clsStaff();
         //capture the StaffID
         AStaff.StaffID = Convert.ToInt32(txtStaffID.Text);
+        AStaff.County = Convert.ToString(lstCounty.Text);
+        AStaff.Street = Convert.ToString(txtStreet.Text);
+        AStaff.PostCode = Convert.ToString(txtPostcode.Text);
+        AStaff.DateOfBirth = Convert.ToString(txtDOB.Text);
+        AStaff.FirstName = Convert.ToString(txtFirstName.Text);
+        AStaff.LastName = Convert.ToString(txtLastName.Text);
+        AStaff.Gender = Convert.ToBoolean(lstGender.Text);
+        AStaff.Telephone = Convert.ToInt32(txtTelephone.Text);
+        AStaff.Active = Convert.ToBoolean(Active.Text);
         //store the staff in the session object 
         Session["AStaff"] = AStaff;
         //redirect to the viewer page
--- PhoneFrontOffice/Default.aspx
+++ PhoneFrontOffice/Default.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:ListBox ID="lstStaff" runat="server" style="z-index: 1; left: 10px; top: 34px; position: absolute; height: 247px; width: 257px"></asp:ListBox>
     </form>
 </body>
 </html>
