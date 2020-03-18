6fe3d5eb49851ec9ac2a94626165c827064bade9
Mohamed Mohamed <P2584197@my365.dmu.ac.uk>
2020-03-18 00:19:15 +0000

    struggled a bit but managed to edit all the errors

--- Phone Testing/tstSupplier.cs
+++ Phone Testing/tstSupplier.cs
@@ -7,11 +7,74 @@ namespace Phone_Testing
     [TestClass]
     public class tstSupplier
     {
+        public tstSupplier()
+        {
+        }
+
         [TestMethod]
         public void InstanceOk()
         {
             clsSupplier ASupplier = new clsSupplier();
             Assert.IsNotNull(ASupplier);
         }
+
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //comment
+            clsSupplier ASupplier = new clsSupplier();
+            //comment
+            Boolean TestData = true;
+            //comment
+            ASupplier.Active = TestData;
+            //comment
+            Assert.AreEqual(ASupplier.Active, TestData);
+        }
+        [TestMethod]
+        public void SupplierIDPropertyOK()
+        {
+            //comment
+            clsSupplier ASupplier = new clsSupplier();
+            //comment
+            Int32 TestData = 1;
+            //comment
+            ASupplier.SupplierID = TestData;
+            //comment
+            Assert.AreEqual(ASupplier.SupplierID, TestData);
+        }
+        [TestMethod]
+        public void ReferencePropertyOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            //comment
+            Int32 TestData = 128;
+            //comment
+            ASupplier.Reference = TestData;
+            //comment
+            Assert.AreEqual(ASupplier.Reference, TestData);
+        }
+        [TestMethod]
+        public void DescriptionPropertyOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            //comment
+            string TestData = "Samsung";
+            //comment
+            ASupplier.Description = TestData;
+            //comment
+            Assert.AreEqual(ASupplier.Description, TestData);
+        }
+        [TestMethod]
+        public void CategoryPropertyOK()
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            //comment
+            string TestData = "Phone";
+            //comment
+            ASupplier.Category = TestData;
+            //comment
+            Assert.AreEqual(ASupplier.Category, TestData);
+        }
     }
+
 }
--- PhoneClasses/clsSupplier.cs
+++ PhoneClasses/clsSupplier.cs
@@ -2,5 +2,11 @@
 {
     public class clsSupplier
     {
+        public bool Active { get; set; }
+        public int SupplierID { get; set; }
+        public int Reference { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string TestData { get; set; }
     }
 }
